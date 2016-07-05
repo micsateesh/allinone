4e8d53821d9cea0126fcdc5ba9e65d21c649ef53
micsateesh <mic.sateesh@gmail.com>
2016-07-05 22:32:32 +0530

    complete mp3 video

---
+++ test/controllers/mp3_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Mp3ControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/mp3_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Mp3HelperTest < ActionView::TestCase
+end
